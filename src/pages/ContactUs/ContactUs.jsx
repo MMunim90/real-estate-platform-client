25dd671c677ffdcc47cbd475cb01112293f4a82f
MMunim <mmunim9.9.01@gmail.com>
2025-08-05 11:01:24 +0600

    added title on contact page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { FaPhoneAlt } from 'react-icons/fa';
-import { motion } from 'framer-motion';
+import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { Helmet } from "react-helmet-async";
 
 const Contact = () => {
   return (
     <div className="min-h-screen flex items-center justify-center px-4 py-12">
+      <Helmet>
+        <title>Contact | BrickBase</title>
+      </Helmet>
       <div className="text-center mb-12 w-full">
         <h2 className="text-3xl font-bold mb-10">GET IN TOUCH</h2>
 
@@ -13,20 +17,25 @@ const Contact = () => {
           <motion.div
             whileHover={{
               scale: 1.02,
-              boxShadow: '0 0 0 4px rgba(59,130,246,0.4)',
+              boxShadow: "0 0 0 4px rgba(59,130,246,0.4)",
             }}
             transition={{ duration: 0.4 }}
             className="rounded overflow-hidden bg-cover bg-center relative bg-[url('https://m.cbhomes.com/p/967/872993/2239676239eD45a/original.jpg')]"
           >
             <div className="bg-black/10 backdrop-blur p-8 h-full lg:h-[400px] flex flex-col justify-between rounded">
               <div className="text-center w-full m-auto">
-                <h3 className="text-xl lg:text-3xl font-semibold text-white mb-2">Advertising Solutions</h3>
+                <h3 className="text-xl lg:text-3xl font-semibold text-white mb-2">
+                  Advertising Solutions
+                </h3>
                 <p className="text-sm text-gray-100 mb-4">
-                  Find out more about our advertising solutions as a CRE owner or broker.
+                  Find out more about our advertising solutions as a CRE owner
+                  or broker.
                 </p>
                 <div className="flex items-center justify-center text-blue-400 font-medium space-x-2 mb-4">
                   <FaPhoneAlt className="text-blue-400" />
-                  <a href="tel:2253594875" className="hover:underline">225-359-4875</a>
+                  <a href="tel:2253594875" className="hover:underline">
+                    225-359-4875
+                  </a>
                 </div>
               </div>
               <button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
@@ -39,20 +48,25 @@ const Contact = () => {
           <motion.div
             whileHover={{
               scale: 1.02,
-              boxShadow: '0 0 0 4px rgba(59,130,246,0.4)',
+              boxShadow: "0 0 0 4px rgba(59,130,246,0.4)",
             }}
             transition={{ duration: 0.4 }}
             className="rounded overflow-hidden bg-cover bg-center relative bg-[url('https://www.bproperty.com/blog/wp-content/uploads/2021/04/house-1867187_1920.jpg')]"
           >
             <div className="bg-black/10 backdrop-blur p-8 h-full lg:h-[400px] flex flex-col justify-between rounded">
               <div className="text-center w-full m-auto">
-                <h3 className="text-xl lg:text-3xl font-semibold text-white mb-2">Customer Support</h3>
+                <h3 className="text-xl lg:text-3xl font-semibold text-white mb-2">
+                  Customer Support
+                </h3>
                 <p className="text-sm text-gray-100 mb-4">
-                  Manage account, billing inquiries, property questions, support team, and more.
+                  Manage account, billing inquiries, property questions, support
+                  team, and more.
                 </p>
                 <div className="flex items-center justify-center text-blue-400 font-medium space-x-2 mb-4">
                   <FaPhoneAlt className="text-blue-400" />
-                  <a href="tel:4876554545" className="hover:underline">487-655-4545</a>
+                  <a href="tel:4876554545" className="hover:underline">
+                    487-655-4545
+                  </a>
                 </div>
               </div>
               <button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
